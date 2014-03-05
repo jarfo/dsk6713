fbf4035a252e51aaba58b64ea01d2f6a6064dbc5
adrian <jose.fonollosa@upc.edu>
2014-03-05 14:54:32 +0100

    RTDSP

--- practica4/block.h
+++ practica4/block.h
@@ -1,9 +1,3 @@
 #define N 			1000
 #define L_FIR 		129     // Orden del filtro FIR + 1
 #define L_IIR 		1     	// Orden del filtro IIR + 1
-
-typedef struct {			// Estructura para filtrado IIR
-	float b[L_IIR];			// Coeficientes b del numerador
-	float a[L_IIR];			// Coeficientes a del denominador
-	float u[L_IIR];			// Registros de desplazamiento del IIR de orden 2
-} filter_iir2;
